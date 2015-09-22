4790f278caddb3f860a302e06d7b31cb59f96760
ZishanAhmedOnik <zisanik@gmail.com>
2015-09-22 10:16:27 +0600

    first commit

---
+++ HelloGitHub/HelloGitHub/HelloGitHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHub
+{
+    class HelloGitHub
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+            Console.ReadLine();
+        }
+    }
+}
